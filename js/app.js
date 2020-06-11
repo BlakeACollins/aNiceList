4cd8f888180f5881cfeb8cf595478abf906af5ab
Blake Collins <blakeac369@gmail.com>
2020-06-11 11:20:25 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -67,3 +67,16 @@ function clearTask(){
     }
 }
 
+//Filter task 
+function filterTask(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(
+        function(task){
+            const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) !=-1){
+                task.style.display = 'block';
+            }else{
+                task.style.display = 'none';
+        }
+        });
+}
